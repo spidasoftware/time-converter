2d52c90b944ee4ab6e71e7472f3ff2451f237ad8
Timothy Overly <timothy.overly@spidasoftware.com>
2016-07-08 15:03:33 -0400

    update employee map

--- index.js
+++ index.js
@@ -23,10 +23,12 @@ var curr = new Date; // get current date
 var first = curr.getDate() - curr.getDay() - 6; // First day is the day of the month - the day of the week
 var last = first + 6; // last day is the first day + 6
 
-//Can run report on monday/tuesday
-var firstday = dateFormat(Date.last().monday(), "yyyymmdd");
-if (Date.today().is().tuesday()) {
-  firstday = dateFormat(Date.last().monday().add(-7).days(), "yyyymmdd");
+var firstday
+
+if (Date.today().is().monday()) {
+  firstday = dateFormat(Date.last().monday(), "yyyymmdd");
+}else{
+  firstday = dateFormat(Date.last().monday().add(-21).days(), "yyyymmdd");
 }
 var lastday = dateFormat(Date.last().sunday(), "yyyymmdd");
 
@@ -39,7 +41,7 @@ var allUsers = [];
 var entriesByUser = function(person, callback) {
 
   //Comment back in to see the users id and meail
-  //console.log(person.user.email+": "+person.user.id)
+  // console.log(person.user.email+": "+person.user.id)
   allUsers[person.user.id] = person.user.email
 
   Reports.timeEntriesByUser({
@@ -48,7 +50,7 @@ var entriesByUser = function(person, callback) {
     to: lastday
   }, function(err, timeEntires) {
     //Handel any error
-    if (err && err.status == "404") {
+    if (err && (err.status || err.message)) {
       console.error(JSON.stringify(err));
     } else if (err) {
       throw new Error(err);
@@ -71,9 +73,21 @@ var entriesByUser = function(person, callback) {
   });
 }
 
-var expensesByDate = function(callback) {
-
+var convertArraytoTIME = function(lineArray){
+  var line = "";
+  //Employee number is the first 6 characters
+  line += ("      " + lineArray[0]).slice(-6);
+  //The fields we do not use and are the next 44 characters
+  line += new Array(45).join( " " );
+  line += lineArray[1]; //D or E
+  line += ("  " + lineArray[2]).slice(-2); //Earn Code
+  line += new Array(10).join( " " ); // Rate
+  line += ("        " + lineArray[3]).slice(-8); //Hours
+  line += new Array(13).join( " " ); // Next 5 date fields
+  line += ("        " + lineArray[4]).slice(-9); //Amount
+  return line;
 }
+
 
 ExpenseCategories.list({}, function(err, expenseCategories) {
 
@@ -98,8 +112,6 @@ ExpenseCategories.list({}, function(err, expenseCategories) {
           if (employeeId) {
 
             var lineToWrite = [employeeId];
-            lineToWrite.push("") //Override Department Number
-
             var taskId = taskIdMap[timeEntry.day_entry.task_id];
             lineToWrite.push("E"); //D or E
             if (taskId) {
@@ -107,13 +119,13 @@ ExpenseCategories.list({}, function(err, expenseCategories) {
             } else {
               lineToWrite.push("1");
             }
-            lineToWrite.push("") //Override Rate
             lineToWrite.push(timeEntry.day_entry.hours) //hours
             lineToWrite.push("") //Amount
-            fs.appendFileSync('reports/TIME0002-' + firstday + "-" + lastday + '.txt', lineToWrite.join("\t") + "\n");
+            fs.appendFileSync('reports/TIME0002-' + firstday + "-" + lastday + '.txt', convertArraytoTIME(lineToWrite) + "\n");
           }
         })
 
+        //Process all the expenses that were retrieved
         _.each(allExpenses, function(expense) {
           if (!expense.expense.is_closed) {
             console.error("" + allUsers[expense.expense.user_id] + " has unapproved expense")
@@ -123,17 +135,15 @@ ExpenseCategories.list({}, function(err, expenseCategories) {
           if (employeeId) {
 
             var lineToWrite = [employeeId];
-            lineToWrite.push("") //Override Department Number
             var reimbursible = _.find(expenseCategories, function(expenseCategory){
-              return expenseCategory.expense_category.name.indexOf("Reimbursable")>=0
+              return expenseCategory.expense_category.name.indexOf("Reimbursable")>=0 && expenseCategory.expense_category.id==expense.expense.expense_category_id
             });
             if(reimbursible){
               lineToWrite.push("D"); //D or E
               lineToWrite.push("R1");
-              lineToWrite.push("") //Override Rate
               lineToWrite.push("") //hours
               lineToWrite.push(expense.expense.total_cost) //Amount
-              fs.appendFileSync('reports/TIME0002-' + firstday + "-" + lastday + '.txt', lineToWrite.join("\t") + "\n");
+              fs.appendFileSync('reports/TIME0002-' + firstday + "-" + lastday + '.txt', convertArraytoTIME(lineToWrite) + "\n");
             }
           }
         });
